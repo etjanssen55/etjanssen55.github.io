b2b9f998b3d010638e3b5275c50ac60417d5360f
Ethan Janssen <500152368@fvtc.edu>
2024-09-21 22:35:12 -0500

    Added Welcome div

--- site.js
+++ site.js
@@ -1,1 +1,25 @@
-console.log('Console Log Test')
+const hours = new Date().getHours() // get the current hour
+
+let isMorning = hours >= 4 && hours < 12 // is it morning?
+let isAfternoon = hours >= 12 && hours < 17 // is it afternoon?
+let isEvening = hours >= 17 || hours < 4 // is it evening?
+
+const welcome = document.querySelector('#welcome')
+
+if(isMorning == true){
+    welcome.textContent = 'Good Morning!'
+}
+
+else if (isAfternoon == true){
+    welcome.textContent = 'Good Afternoon!'
+}
+
+else if (isEvening == true){
+    welcome.textContent = 'Good Evening!'
+}
+
+else{
+    welcome.textContent = 'What year is it?!'
+}
+
+console.log(hours.toString())
